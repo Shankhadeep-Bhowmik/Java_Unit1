fda44a67d54259f2a28389cdf451e461e5ab6542
Shankhadeep-Bhowmik <bsulekha0371@gmail.com>
2025-11-25 20:46:54 +0530

    Adding file 2

---
+++ java_files/VariableDemo.java
@@ -0,0 +1,12 @@
+public class VariableDemo {
+  public static void main(String[] args) {
+    int age = 25; // integer
+    double salary = 55000.75; // double
+    char grade = 'A'; // character
+    boolean isStudent = false; // boolean
+    System.out.println("Age: "+age);
+    System.out.println("Salary: "+salary);
+    System.out.println("Grade: "+grade);
+    System.out.println("Student: "+isStudent);  
+  }
+}
