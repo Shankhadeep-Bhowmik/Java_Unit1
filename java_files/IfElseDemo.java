2d63e8f6d890564e14c2298cd09517305f82c6e6
Shankhadeep-Bhowmik <bsulekha0371@gmail.com>
2025-11-25 21:15:09 +0530

    Adding file 5

---
+++ java_files/IfElseDemo.java
@@ -0,0 +1,10 @@
+public class IfElseDemo {
+  public static void main(String[] args) {
+    int number = 10;
+    if (number%2==0) {
+      System.out.println("Even");
+    }else{
+      System.out.println("Odd");
+    }
+  }
+}
